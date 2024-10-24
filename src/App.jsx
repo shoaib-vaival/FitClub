06fdf5fb9f7a5aa86b070557745071c851bf140e
Shoaib <shoaib3430@gmail.com>
2024-10-24 14:25:59 +0000

    Clean code

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
-import './App.css';
-import Hero from './Components/Hero/Hero';
-import Plans from './Components/Plans/Plans';
-import Programs from './Components/Programs/Programs';
-import Reasons from './Components/Reasons/Reasons';
-import Testimonials from './Components/Testimonials/Testimonials';
-import Footer from './Components/Footer/Footer';
-import Join from './Components/Join/Join';
-import Contact from './Components/Contact/Contact';
+import "./App.css";
+import Hero from "./Components/Hero/Hero";
+import Plans from "./Components/Plans/Plans";
+import Programs from "./Components/Programs/Programs";
+import Reasons from "./Components/Reasons/Reasons";
+import Testimonials from "./Components/Testimonials/Testimonials";
+import Footer from "./Components/Footer/Footer";
+import Join from "./Components/Join/Join";
 
 function App() {
   return (
     <div className="App">
-      <Hero/>
-      <Programs/>
-      <Reasons/>
-      <Plans/>
-      <Testimonials/>
-      <Join/>
-      {/* <Contact/> */}
-      <Footer/>
+      <Hero />
+      <Programs />
+      <Reasons />
+      <Plans />
+      <Testimonials />
+      <Join />
+      <Footer />
     </div>
   );
 }
